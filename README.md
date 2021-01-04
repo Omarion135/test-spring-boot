239301fddc6814702127b3392e4f6f6777fcd2c9
c11562 <omar.naghmouchi@bnpparibas.com>
2021-01-04 17:50:15 +0100

    init

---
+++ README.md
@@ -0,0 +1,8 @@
+<p>
+0 - créer une branche git </br>
+1 - lancer clean install </br>
+2 - lancer l'application </br>
+3 - corriger les erreur de runtime </br>
+4 - Appeler le service qui permet de recupérer tous les 'book' -  corriger le probléme</br>
+4 - Implementer un service qui permet de recupérer un 'book' par son id </br>
+</p>
---
+++ src/main/java/com/bnp/BookController.java
@@ -0,0 +1,68 @@
+package com.bnp;
+
+import com.bnp.error.BookNotFoundException;
+import com.bnp.error.BookUnSupportedFieldPatchException;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.util.StringUtils;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Map;
+
+@RestController
+public class BookController {
+
+    private BookRepository repository;
+
+    @GetMapping("/books")
+    List<Book> findAll() {
+        return repository.findAll();
+    }
+
+    @PostMapping("/books")
+    @ResponseStatus(HttpStatus.CREATED)
+    Book newBook(@RequestBody Book newBook) {
+        return repository.save(newBook);
+    }
+
+    @PutMapping("/books/{id}")
+    Book saveOrUpdate(@RequestBody Book newBook, @PathVariable Long id) {
+
+        return repository.findById(id)
+                .map(x -> {
+                    x.setName(newBook.getName());
+                    x.setAuthor(newBook.getAuthor());
+                    x.setPrice(newBook.getPrice());
+                    return repository.save(x);
+                })
+                .orElseGet(() -> {
+                    newBook.setId(id);
+                    return repository.save(newBook);
+                });
+    }
+
+    @PatchMapping("/books/{id}")
+    Book patch(@RequestBody Map<String, String> update, @PathVariable Long id) {
+
+        return repository.findById(id)
+                .map(x -> {
+
+                    String author = update.get("author");
+                    if (!StringUtils.isEmpty(author)) {
+                        x.setAuthor(author);
+
+                        // better create a custom method to update a value = :newValue where id = :id
+                        return repository.save(x);
+                    } else {
+                        throw new BookUnSupportedFieldPatchException(update.keySet());
+                    }
+
+                })
+                .orElseGet(() -> {
+                    throw new BookNotFoundException(id);
+                });
+
+    }
+
+}
